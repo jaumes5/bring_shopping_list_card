e9e10ef3ab0352e23cff3e60da9d85d572f47226
jaumes5 <jaumes524@gmail.com>
2023-04-14 14:57:47 +0200

    Added hidden overflow

--- dist/bring_shopping_list_card.js
+++ dist/bring_shopping_list_card.js
@@ -68,12 +68,15 @@ class BringShoppingList extends LitElement {
                       max-height: 100%;" 
                 src="https://web.getbring.com/assets/images/items/${item.image}.png">
         </div>
-        <div style="width: 100%;height: 100%;">
+        <div style="width: 100%;height: ${this.config.itemHeight / 2}px";overflow:hidden;">
           <label style="display: block;
                         text-align: center;
                         color: #FFF;
                         font-size: 0.8em;
-                        cursor: pointer;"
+                        cursor: pointer;
+                        max-height: 52%;
+                        overflow: hidden;
+                        text-overflow: ellipsis;"
           >${item.name}</label>
           <label style="display: block;
                         text-align: center;
